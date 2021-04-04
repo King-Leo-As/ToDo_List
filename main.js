41ffde61c1eb4a1eacb4792f4da47c8b1320abe3
Asadbek <asadbek.azimjonov.02@mail.ru>
2021-04-04 10:50:45 +0500

    create html  css js
    create html  css js  and i forgot to push after being done each activity

---
+++ main.js
@@ -0,0 +1,5 @@
+const formElement = document.querySelector(".form")
+const inputElement = document.querySelector('.form__input-name')
+const timeELement = document.querySelector('.form__input-time')
+
+let todo = []
