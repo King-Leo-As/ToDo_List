423abb3db6faa060c8818d21f47a3b032ffd4f3a
Asadbek <asadbek.azimjonov.02@mail.ru>
2021-04-04 16:39:33 +0500

    edit
    edit

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const inputElement = document.querySelector('.form__input-name')
 const timeELement = document.querySelector('.form__input-time')
 
 let todo = []
+
+formElement.addEventListener('submit', evt =>{
+    evt.preventDefault()
+    if(!(inputElement.value && timeELement.value)) return alert('Vaqt belgilamadingiz')
+    console.log(inputElement.value, timeELement.value)
+})
